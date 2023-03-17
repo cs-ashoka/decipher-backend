49ceb8ae7afe48eca0e0f697c13a32508a3cb67c
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2023-03-18 03:39:25 +0530

    init

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require("express");
+import "dotenv/config";
+import cors from "cors";
+import express, { json } from "express";
+
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
+app.use(json());
+
+app.get('/', (req, res) => {
+    res.send("Decipher")
+})
  
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
