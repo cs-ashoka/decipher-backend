c018218e9d99a783c538673e99719cdb20fd0356
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-02-22 16:45:01 +0530

    Change samesite settings on cookie

--- api/index.js
+++ api/index.js
@@ -53,7 +53,7 @@ app.use(
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "lax",
+      sameSite: "none",
       domain:
         process.env.NODE_ENV === "production"
           ? "decipher.berlm.me"
